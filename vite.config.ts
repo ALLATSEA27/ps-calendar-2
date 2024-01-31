bae233026123e2b22f930148719a0da5b65a3823
“Ajinkya <ajinkya.an@zest.ai>
2024-01-31 13:36:51 +0530

    gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const repoName = "ps-calendar-2";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 3000,
   },
   plugins: [react()],
+  base: `/${repoName}/`,
 });
